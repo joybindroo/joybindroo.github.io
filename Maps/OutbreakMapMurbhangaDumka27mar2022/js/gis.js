1e11609c118d618784377fed7ee31edd58e697f1
Joy Bindroo <joybindroo@gmail.com>
2023-04-12 17:49:14 +0530

    updated mb map

--- Maps/OutbreakMapMurbhangaDumka27mar2022/js/gis.js
+++ Maps/OutbreakMapMurbhangaDumka27mar2022/js/gis.js
@@ -754,17 +754,3 @@ function fnc__specialcol_(values, context) {
 function fnc_project_color(values, context) {
     return false;
 };
-
-
-
-function exp_label_MurbhangaVL_2_eval_expression(context) {
-    // 
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return null;
-    } else {
-        return null;
-    }
-}
