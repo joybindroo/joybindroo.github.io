c02ebdc05da24ff6d503794e1f95231bba7ed9b7
Joy Bindroo <joybindroo@gmail.com>
2024-03-12 18:24:59 +0530

    added ka type A vil map

---
+++ Maps/KA_Map_TypeA_Vills_2024_03_12/data/PatnaBihar_4.js
@@ -0,0 +1,1 @@
+var json_PatnaBihar_4 = {"type":"FeatureCollection","name":"PatnaBihar_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","name":"Patna"},"geometry":{"type":"Point","coordinates":[85.110729545160396,25.628261600723242]}}]}
