7d262261266d63a2cdb4ea5166002c6cbd20455d
Joy Bindroo <joybindroo@gmail.com>
2024-10-08 20:33:05 +0530

    Create script.js

---
+++ bsi/script.js
@@ -0,0 +1,23 @@
+// Tab switching logic
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Display the first tab by default
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementsByClassName('tablinks')[0].click();
+});
+
+// Three.js 3D background logic
+var scene = new THREE.Scene();
+var
